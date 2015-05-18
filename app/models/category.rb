c9eb21b0843e48167316e8b6ab7245a846db9cdd
john-karanja <arakanj@gmail.com>
2015-05-18 11:19:00 +0300

    changed image size in paperclip

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
-  has_attached_file :image, styles: { medium: "200x200#" }
+  has_attached_file :image, styles: { medium: "300x300#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
